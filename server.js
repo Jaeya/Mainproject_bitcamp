c729fbf63840902e6267bfa8a4787573f93ad892
Jaeya <yi2228@naver.com>
2021-10-04 16:33:35 +0900

    Complete

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const formatMessage = require('./utils/messages')
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users')
 
 
 const app = express();
@@ -15,26 +16,58 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const botName = 'ChatCord Bot';
+const botName = 'BITCAMP Bot';
 
 // Run when client connects                                        // socket.emit 클라이언트 단일  / broadcast 클라이언트 제외 모두 / io.emit 모두
 io.on('connection', socket => {
-    
-    // Welcome current user
-    socket.emit('message', formatMessage(botName,'Welcome to ChatCord!'))
+
+    socket.on('joinRoom', ({username, room}) =>{
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room)
+
+        
+         
+        // Welcome current user
+    socket.emit('message', formatMessage(botName,'Welcome to BIT Occasion!'))
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName,' A user has joined the chat'));
+    socket.broadcast
+        .to(user.room)
+        .emit('message', formatMessage(botName,`${user.username} 님이 채팅방에 들어오셨습니다.`));
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', (msg) =>{
+        const user = getCurrentUser(socket.id);
+
+        io
+        .to(user.room)
+        .emit('message', formatMessage(user.username, msg));
+    
+    });
 
     // Runs when client disconnects
     socket.on('disconnect', ()=> {
-        io.emit('message', formatMessage(botName,'A user has left the chat'));
+        const user = userLeave(socket.id);
 
-    });
-    // Listen for chatMessage
-    socket.on('chatMessage', (msg) =>{
-        io.emit('message', formatMessage('USER', msg));
-    
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName,`${user.username} 님이 채팅방을 나가셨습니다.`));
+
+              // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+        }
 
     });
     
